ace55b16bcddd8f17a30620a8793a27255f727cd
Yusuf AKIN <yusuf.akn2017@hotmail.com>
2023-12-07 17:54:12 +0300

    :rocket: First Commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+// TEMPLATE ENGINE
+app.set('view engine', 'ejs');
+
+// MIDDLEWARES
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+    res.status(200).render('index', {
+        page_name: "index",
+    });
+});
+app.get('/about', (req, res) => {
+    res.status(200).render('about', {
+        page_name: "about",
+    });
+});
+
+app.listen(port, () => {
+    console.log(`App listen port ${port}`);
+});
